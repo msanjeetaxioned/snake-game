28b4566e6ce337585027ba401d44e05f6c47d82c
Sanjeet Mistry <sanjeetm@axioned.com>
2021-12-09 17:49:56 +0530

    Changes: - Add Support for 'Left' and 'Down' - Add 4 more parts to snake (Total 8 now from 4)

--- js/snake.js
+++ js/snake.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(event) {
     const snakeContainerDimensions = {width: 1000, height: 500};
-    const snakeSize = {width: 40, height: 10};
+    const snakeSize = {width: 80, height: 10};
     const snakePartSize = {width: 10, height: 10};
     const positionIncrement = 10;
     const directions = ["left", "right", "up", "down"];
@@ -15,26 +15,26 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
     document.addEventListener("keydown", function(event) {
         switch(event.key) {
-            // case "ArrowLeft": 
-            //     if(currentDirection != "right") {
-            //         currentDirection = directions[0];
-            //     }
-            //     break;
+            case "ArrowLeft": 
+                if(currentDirection != "right" && currentDirection != "left") {
+                    currentDirection = directions[0];
+                }
+                break;
             case "ArrowRight":
-                if(currentDirection != "left") {
+                if(currentDirection != "left" && currentDirection != "right") {
                     currentDirection = directions[1];
                 }
                 break;
             case "ArrowUp":
-                if(currentDirection != "down") {
+                if(currentDirection != "down" && currentDirection != "up") {
                     currentDirection = directions[2];
                 }
                 break;
-            // case "ArrowDown": 
-            // if(currentDirection != "up") {
-            //     currentDirection = directions[3];
-            // }
-            // break;
+            case "ArrowDown": 
+            if(currentDirection != "up" && currentDirection != "down") {
+                currentDirection = directions[3];
+            }
+            break;
         }
     });
 
@@ -48,19 +48,35 @@ document.addEventListener('DOMContentLoaded', function(event) {
         for(i = snakeParts.length - 1; i >= 0; i--) {
             if(i == 0) {
                 if(currentDirection == "right") {
-                    if(parseInt(snakeParts[i].style.left) + positionIncrement == snakeContainerDimensions.width) {
+                    if(parseInt(snakeParts[i].style.left) + positionIncrement >= snakeContainerDimensions.width) {
                         snakeParts[i].style.left = "0px";
                     }
                     else {
                         snakeParts[i].style.left = parseInt(snakeParts[i].style.left) + positionIncrement + "px";
                     }
                 }
+                if(currentDirection == "left") {
+                    if(parseInt(snakeParts[i].style.left) <= 0) {
+                        snakeParts[i].style.left = snakeContainerDimensions.width - positionIncrement + "px";
+                    }
+                    else {
+                        snakeParts[i].style.left = parseInt(snakeParts[i].style.left) - positionIncrement + "px";
+                    }
+                }
                 else if(currentDirection == "up") {
-                    if(parseInt(snakeParts[i].style.top) == 0) {
-                        snakeParts[i].style.top = snakeContainerDimensions.height + "px";
+                    if(parseInt(snakeParts[i].style.top) <= 0) {
+                        snakeParts[i].style.top = snakeContainerDimensions.height - positionIncrement + "px";
                     }
                     else {
                         snakeParts[i].style.top = parseInt(snakeParts[i].style.top) - positionIncrement + "px";
+                    }
+                }
+                else if(currentDirection == "down") {
+                    if(parseInt(snakeParts[i].style.top) >= snakeContainerDimensions.height) {
+                        snakeParts[i].style.top = "0px";
+                    }
+                    else {
+                        snakeParts[i].style.top = parseInt(snakeParts[i].style.top) + positionIncrement + "px";
                     }
                 }
             }
